4dd58460434c07116ed42c08ef61071e0c3cb55e
Hexaa <me@hexagonn.my.id>
2025-11-26 13:21:01 +0700

    feat(blog): responsive banner

---
+++ src/app/blog/banner.client.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { PlusSeparator } from "@/components/ui/plus-separator";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { GrainGradient } from "@paper-design/shaders-react";
+
+export function HeaderBanner() {
+  const isMobile = useIsMobile();
+
+  return (
+    <section className="w-full border-separator/10 border-t">
+      <div className="inner relative flex border-separator/10 border-x">
+        <PlusSeparator
+          position={["top-left", "top-right", "bottom-left", "bottom-right"]}
+          main={{ className: "z-20" }}
+        />
+        <GrainGradient
+          height={250}
+          colors={["#c6750c", "#beae60", "#d7cbc6"]}
+          colorBack="#ffffff00"
+          softness={0.7}
+          intensity={0.15}
+          noise={isMobile ? 0.25 : 0.5}
+          shape="wave"
+          speed={0.7}
+          scale={isMobile ? 1 : 2.5}
+          offsetX={1}
+          offsetY={0.6}
+          className="w-full bg-background/20"
+        />
+        <div className="absolute top-0 right-0 bottom-0 left-0 z-10 h-full w-full text-white mix-blend-difference">
+          <div className="mt-12 ml-10 flex h-full flex-col">
+            <h2 className="text-2xl md:text-4xl">hexaa's blog.</h2>
+            <p className="text-sm md:text-base">
+              here goes another yap and yap
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,8 +2,8 @@ import { PathUtils } from "fumadocs-core/source";
 import { blog } from "@/lib/source";
 
 import { PlusSeparator } from "@/components/ui/plus-separator";
-import { GrainGradient } from "@paper-design/shaders-react";
 import Link from "next/link";
+import { HeaderBanner } from "./banner.client";
 
 export default function BlogPage() {
   const posts = [...blog.getPages()].sort(
@@ -14,34 +14,7 @@ export default function BlogPage() {
 
   return (
     <main className="mt-16">
-      <section className="w-full border-separator/10 border-t">
-        <div className="inner relative flex border-separator/10 border-x">
-          <PlusSeparator
-            position={["top-left", "top-right", "bottom-left", "bottom-right"]}
-            main={{ className: "z-20" }}
-          />
-          <GrainGradient
-            height={250}
-            colors={["#c6750c", "#beae60", "#d7cbc6"]}
-            colorBack="#ffffff00"
-            softness={0.7}
-            intensity={0.15}
-            noise={0.5}
-            shape="wave"
-            speed={0.7}
-            scale={2.5}
-            offsetX={1}
-            offsetY={0.6}
-            className="w-full bg-background/20"
-          />
-          <div className="absolute top-0 right-0 bottom-0 left-0 z-10 h-full w-full text-white mix-blend-difference">
-            <div className="mt-12 ml-10 flex h-full flex-col">
-              <h2 className="text-4xl">hexaa's blog.</h2>
-              <p>here goes another yap and yap</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <HeaderBanner />
       <section className="w-full border-separator/10 border-y">
         <div className="inner relative grid grid-cols-1 gap-2 border-separator/10 border-x px-2 py-16 md:grid-cols-2 lg:grid-cols-3">
           {posts.map((post) => (
